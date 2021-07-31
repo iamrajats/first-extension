13de3a7d389ca7d7c465753559078070054e68ee
rajat.srivastava <srivastava.rajat312@gmail.com>
2021-07-31 10:23:51 +0530

    Seed apod project from cookiecutter

---
+++ jupyterlab_apod/src/index.ts
@@ -0,0 +1,29 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { requestAPI } from './handler';
+
+/**
+ * Initialization data for the jupyterlab_apod extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab_apod:plugin',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    console.log('JupyterLab extension jupyterlab_apod is activated!');
+
+    requestAPI<any>('get_example')
+      .then(data => {
+        console.log(data);
+      })
+      .catch(reason => {
+        console.error(
+          `The jupyterlab_apod server extension appears to be missing.\n${reason}`
+        );
+      });
+  }
+};
+
+export default plugin;
